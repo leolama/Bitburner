b10706a9f5a0ab3e9c7aced3e1bfbdfe14e108e5
leolama <leolama2@hotmail.co.uk>
2022-01-12 17:31:14 +0000

    removed old file

--- autoexec/autocreate.js
+++
@@ -1,7 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-	ns.tprint(ns.getPlayer().hacking);
-	ns.tprint(ns.getPlayer().hacking_exp);
-}
-
-//BruteSSH.exe: 50 * FTPCrack.exe: 100 * relaySMTP.exe: 250 * HTTPWorm.exe: 500 * SQLInject.exe: 750
