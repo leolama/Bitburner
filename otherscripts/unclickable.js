a2ad4dd0f90cb15a1f43b74ebcaf50070cfa1b55
leolama <leolama2@hotmail.co.uk>
2022-01-13 17:05:46 +0000

    exploit scripts

---
+++ otherscripts/unclickable.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	let div = eval("document.getElementById('unclickable')");
+
+	div.style.display = "block";
+	div.style.visibility = "visible";
+	div.style.backgroundColor = 'red';
+	div.addEventListener('mouseup', function (e) {hideElem(div);});
+}
+
+function hideElem(elem) {
+	elem.style.display = "none";
+	elem.style.visibility = "hidden";
+	elem.style.backgroundColor = '';
+}
