d44fe47e2dbabd61f80aad00eb5dc155af437ba9
leolama <leolama2@hotmail.co.uk>
2022-01-13 17:06:09 +0000

    removing dead files

--- autoexec/autofaction.js
+++ autoexec/autofaction.js
@@ -1,11 +1,20 @@
+//https://github.com/leolama/Bitburner
 /** @param {NS} ns **/
-function countTools(numTools) {
-
-	return numTools;
-}
-
 export async function main(ns) {
-	var numTools = countTools();
+	let numTools = countTools();
 
 	ns.tprint(numTools);
+}
+
+function countTools(numTools) {
+	let hTools = ["brutessh.exe","ftpcrack.exe","relaysmtp.exe","httpworm.exe","sqlinject.exe"]
+	let numTools = 0
+
+	for (let i = 0;i < 4; ++i) {
+		if (ns.fileExists(hTools[i])) {
+			++numTools;
+		}
+	}
+
+	return numTools;
 }
