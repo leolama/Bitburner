6bd10eecddf82d5830df2cbd81d697edc9b87842
leolama <leolama2@hotmail.co.uk>
2022-01-14 01:35:53 +0000

    util

--- autoexec/autofaction.js
+++ autoexec/autofaction.js
@@ -1,20 +1,8 @@
 //https://github.com/leolama/Bitburner
+import { countTools } from "util"
 /** @param {NS} ns **/
 export async function main(ns) {
 	let numTools = countTools();
 
 	ns.tprint(numTools);
-}
-
-function countTools(numTools) {
-	let hTools = ["brutessh.exe","ftpcrack.exe","relaysmtp.exe","httpworm.exe","sqlinject.exe"]
-	let numTools = 0
-
-	for (let i = 0;i < 4; ++i) {
-		if (ns.fileExists(hTools[i])) {
-			++numTools;
-		}
-	}
-
-	return numTools;
 }
--- util.js
+++ util.js
@@ -1,5 +1,13 @@
 //https://github.com/leolama/Bitburner
 /** @param {NS} ns **/
-export async function main(ns) {
+export function countTools(ns) {
+	let hTools = ["brutessh.exe","ftpcrack.exe","relaysmtp.exe","httpworm.exe","sqlinject.exe"]
+	let numTools = 0
 
+	for (let i = 0;i < 4; ++i) {
+		if (ns.fileExists(hTools[i])) {
+			++numTools;
+		}
+	}
+	return numTools;
 }
