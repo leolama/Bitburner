efc299d678b4a26f3f7a9c5ded0717d3a9e10fb7
leolama <leolama2@hotmail.co.uk>
2022-01-14 02:33:57 +0000

    origins of util

--- util.js
+++ util.js
@@ -1,13 +1,25 @@
-//https://github.com/leolama/Bitburner
 /** @param {NS} ns **/
 export function countTools(ns) {
-	let hTools = ["brutessh.exe","ftpcrack.exe","relaysmtp.exe","httpworm.exe","sqlinject.exe"]
-	let numTools = 0
+	var programs = ["brutessh.exe","ftpcrack.exe","relaysmtp.exe","httpworm.exe","sqlinject.exe"]
+	var numTools = 0
 
-	for (let i = 0;i < 4; ++i) {
-		if (ns.fileExists(hTools[i])) {
+	for (let prog of programs) {
+		if (ns.fileExists(prog)) {
 			++numTools;
 		}
 	}
 	return numTools;
+}
+
+export function playerStats(ns) {
+	var hp = ns.getPlayer().max_hp;
+	var money = ns.getPlayer().money;
+	var hackingLvl = ns.getPlayer().hacking;
+	var strengthLvl = ns.getPlayer().strength;
+	var defenseLvl = ns.getPlayer().defense;
+	var dexterityLvl = ns.getPlayer().dexterity;
+	var agilityLvl = ns.getPlayer().agility;
+	var charismaLvl = ns.getPlayer().charisma;
+
+	
 }
