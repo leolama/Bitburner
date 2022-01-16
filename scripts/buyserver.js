276c05de6a93ec52144004303750b045fbc025ff
leolama <leolama2@hotmail.co.uk>
2022-01-16 17:53:16 +0000

    changed naming scheme

--- scripts/buyserver.js
+++ scripts/buyserver.js
@@ -1,8 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
     var multipleOf = [2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576];
-    var hName = "custom-server";
     var ram = ns.args[0];
+    var hName = "personal-server-" + ram;
 
     for (let i = 0; i < multipleOf.length; ++i) {
         if (ram === multipleOf[i]) { //if ram equals a multiple of 2
