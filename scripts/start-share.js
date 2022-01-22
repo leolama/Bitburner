babe519f03e70932a742b02a23c9a0c477fb72d2
leolama <leolama2@hotmail.co.uk>
2022-01-22 00:36:01 +0000

    small change

--- scripts/start-share.js
+++ scripts/start-share.js
@@ -3,7 +3,7 @@ export async function main(ns) {
 	var threads;
 	threads = ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
 	threads /= ns.getScriptRam("/scripts/share.js", "home");
-	threads = Math.floor(threads) - 20;
+	threads = Math.floor(threads) - 10; //spares ~40GB RAM
 
 	ns.run("/scripts/share.js", threads);
 }
