a5b12fa06e1a90744fc0a730b6dac5cacd837e16
leolama <leolama2@hotmail.co.uk>
2022-01-25 20:24:49 +0000

    added some tprints

--- managers/faction-manager.js
+++ managers/faction-manager.js
@@ -2,9 +2,10 @@
 import { hackTools, nukeServer } from "util.js";
 
 export async function main(ns) {
-	var factionNames = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"]; //hacking based factions (that I know of so far)
-	var factionHackLvl = ["54", "203", "358", "511"]; //required hacking level
-	var factionProgs = ["1", "2", "3", "4"];
+	ns.print("Script started");
+	const factionNames = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"]; //hacking based factions (that I know of so far)
+	const factionHackLvl = ["54", "203", "358", "511"]; //required hacking level
+	const factionProgs = ["1", "2", "3", "4"]; //number of programs needed to root
 	var numTools = hackTools(ns);
 	var count = 0;
 	var hackingLvl = ns.getPlayer().hacking;
@@ -14,9 +15,14 @@ export async function main(ns) {
 			if (factionHackLvl[count] <= hackingLvl && numTools >= factionProgs[count]) {
 				await nukeServer(ns, factionNames[count]);
 				ns.tprint("Installing a backdoor on " + factionNames[count] + " in 3 seconds");
-				await ns.sleep(3000);
+				await ns.sleep(1000);
+				ns.tprint("Installing a backdoor on " + factionNames[count] + " in 2 seconds");
+				await ns.sleep(1000);
+				ns.tprint("Installing a backdoor on " + factionNames[count] + " in 1 seconds");
+				await ns.sleep(1000);
 				ns.run("src/connect.js", 1, factionNames[count]);
 				await ns.sleep(100);
+				ns.tprint("Installing backdoor...");
 				await ns.installBackdoor();
 				if (ns.getServer(factionNames[count]).backdoorInstalled === true) {
 					++count;
@@ -39,4 +45,5 @@ export async function main(ns) {
 		var numTools = hackTools(ns);
 		var hackingLvl = ns.getPlayer().hacking;
 	}
+	ns.print("Backdoored all faction servers");
 }
