6714db719cbc88184d3931ae012713b3b3422955
leolama <leolama2@hotmail.co.uk>
2022-01-27 00:21:07 +0000

    added comments

--- managers/crime-manager.js
+++ managers/crime-manager.js
@@ -1,3 +1,8 @@
+/*
+	by default does the best crime for stats
+	'money', 'karma' or the crime name as an argument to override the default
+*/
+
 /** @param {import("../.").NS} ns */
 export async function main(ns) {
 	ns.print("Script started");
@@ -19,26 +24,26 @@ export async function main(ns) {
 	];
 	const statNames = ["strength", "defense", "agility", "dexterity"];
 	const possibleArgs = ["money","karma"];
-	const noArg = "noArg";
 
 
 
-	//test each crime with a special thrown-together algorithm
+	//test each crime with a 'special' thrown-together algorithm
 	function crime(arg) {
 		let chance = 0;
 		let time = 0;
 		let stats = 0.0;
 		let optimal = 0;
-		let optimalCrime = "shoplift";
+		let optimalCrime = "heist";
 
-		if (arg = "noArg") {
+		if (arg == "noArg") {
 			for (let i = 0; i < crimeList.length; ++i) {
+				ns.print("optimal: " + optimalCrime);
 				chance = ns.getCrimeChance(crimeList[i]);
-				if (chance < 0.80) {
+				if (chance < 0.80) { //if chance to do crime is under 80% then skip it
 					continue;
 				}
-				time = ns.getCrimeStats(crimeList[i]).time;
-				stats =
+				time = ns.getCrimeStats(crimeList[i]).time; //time to complete the crime
+				stats = //stat exp received if successful
 					ns.getCrimeStats(crimeList[i]).strength_exp +
 					ns.getCrimeStats(crimeList[i]).defense_exp +
 					ns.getCrimeStats(crimeList[i]).dexterity_exp +
@@ -51,7 +56,7 @@ export async function main(ns) {
 			}
 			return optimalCrime;
 		}
-		else if (arg = "money") {
+		else if (arg == "money") {
 			for (let i = 0; i < crimeList.length; ++i) {
 				chance = ns.getCrimeChance(crimeList[i]);
 				if (chance < 0.80) {
@@ -67,7 +72,7 @@ export async function main(ns) {
 			}
 			return optimalCrime;
 		}
-		else if (arg = "karma") {
+		else if (arg == "karma") {
 			for (let i = 0; i < crimeList.length; ++i) {
 				chance = ns.getCrimeChance(crimeList[i]);
 				if (chance < 0.80) {
@@ -85,7 +90,7 @@ export async function main(ns) {
 		}
 
 	}
-
+	
 	async function trainStats() {
 		//get player stats
 		var statLevels = [ns.getPlayer().strength, ns.getPlayer().defense, ns.getPlayer().agility, ns.getPlayer().dexterity];
@@ -102,40 +107,44 @@ export async function main(ns) {
 		ns.stopAction();
 	}
 
+	var str = ns.getPlayer().strength;
+	var def = ns.getPlayer().defense;
+	var agi = ns.getPlayer().agility;
+	var dex = ns.getPlayer().dexterity;
+	var oldKarma = 0;
+	var karma = 0;
+	var busy = 0;
+	var bestCrime;
+
+	//if player stats are under 15 then train at the gym until they aren't
+	if (str < 15 || def < 15 || agi < 15 || dex < 15) {
+		await trainStats();
+	}
+
 	//main
+	oldKarma = karma
 	while (true) {
-		var str = ns.getPlayer().strength;
-		var def = ns.getPlayer().defense;
-		var agi = ns.getPlayer().agility;
-		var dex = ns.getPlayer().dexterity;
-		var oldKarma;
-		var karma;
-		var busy = 0;
-		if (str < 15 || def < 15 || agi < 15 || dex < 15) {
-			await trainStats();
-		}
-		var bestCrime;
-		if (possibleArgs.includes(ns.args[0])) {
+		if (possibleArgs.includes(ns.args[0])) { //checking for money or karma args
 			bestCrime = crime(ns.args[0]);
-		} else if (crimeList.includes(ns.args[0])) {
+		} else if (crimeList.includes(ns.args[0])) { //checking for heistname arg
 			bestCrime = ns.args[0];
 		}
 		else {
-			bestCrime = crime(noArg);
+			bestCrime = crime("noArg");
 		}
+
+		//get and format the time into 24h
 		var date = new Date(Date.now() + ns.getCrimeStats(bestCrime).time);
 		var time = ns.nFormat(date.getHours(), "00") + ":" + ns.nFormat(date.getMinutes(), "00") + ":" + ns.nFormat(date.getSeconds(), "00");
 
-
 		if (!ns.isBusy()) {
-			oldKarma = ns.heart.break();
 			ns.print("Attempting to commit " + bestCrime + ". Expecting finish at " + time);
 			await ns.sleep(ns.commitCrime(bestCrime) - 500);
-			if (!ns.isBusy()) {
+			if (!ns.isBusy()) { //if the player cancels the crime, stop the script
 				ns.print("Cancelled by player");
 				return;
 			}
-			karma = ns.heart.break();
+			karma = ns.heart.break(); //get karma value
 			if (oldKarma > karma) {
 				ns.print("Success!");
 				ns.print("Current karma " + karma);
@@ -146,11 +155,13 @@ export async function main(ns) {
 			ns.print("Player is busy");
 			busy = 0;
 			while (busy < 1) {
-				if (!ns.isBusy()) ++busy;
+				if (!ns.isBusy()) ++busy; //if player is busy then wait a second and try again
 				else {
 					await ns.sleep(1000);
 				}
 			}
 		}
+		await ns.sleep(500);
+		oldKarma = karma;//move karma into another var
 	}
 }
