d49e9ce3bb098f369b770e2627791ccf7069c2bd
leolama <leolama2@hotmail.co.uk>
2022-02-05 17:36:35 +0000

    refined for karma

--- managers/crime-manager.js
+++ managers/crime-manager.js
@@ -1,102 +1,29 @@
-/*
-	by default does the best crime for stats
-	'money', 'karma' or the crime name as an argument to override the default
-*/
+const statNames = ["strength", "defense", "dexterity", "agility"];
 
 /** @param {import("../.").NS} ns */
 export async function main(ns) {
 	ns.print("Script started");
 	ns.disableLog("ALL");
 
-	const crimeList = [
-		"heist",
-		"assassinate",
-		"kidnap and ransom",
-		"grand theft auto",
-		"homicide",
-		"traffick illegal arms",
-		"bond forgery",
-		"deal drugs",
-		"larceny",
-		"mug someone",
-		"rob store",
-		"shoplift",
-	];
-	const statNames = ["strength", "defense", "agility", "dexterity"];
-	const possibleArgs = ["money", "karma"];
-
-	//test each crime with a 'special' thrown-together algorithm
-	function crime(arg) {
-		let chance = 0;
-		let time = 0;
-		let stats = 0.0;
-		let optimal = 0;
-		let optimalCrime = "heist";
-
-		if (arg == "noArg") {
-			for (let i = 0; i < crimeList.length; ++i) {
-				chance = ns.getCrimeChance(crimeList[i]);
-				if (chance < 0.8) {
-					//if chance to do crime is under 80% then skip it
-					continue;
-				}
-				time = ns.getCrimeStats(crimeList[i]).time; //time to complete the crime
-				stats = //stat exp received if successful
-					ns.getCrimeStats(crimeList[i]).strength_exp +
-					ns.getCrimeStats(crimeList[i]).defense_exp +
-					ns.getCrimeStats(crimeList[i]).dexterity_exp +
-					ns.getCrimeStats(crimeList[i]).agility_exp;
-				stats /= time;
-				if (stats > optimal) {
-					optimal = stats;
-					optimalCrime = crimeList[i];
-				}
-			}
-			return optimalCrime;
-		} else if (arg == "money") {
-			for (let i = 0; i < crimeList.length; ++i) {
-				chance = ns.getCrimeChance(crimeList[i]);
-				if (chance < 0.8) {
-					continue;
-				}
-				time = ns.getCrimeStats(crimeList[i]).time;
-				stats = ns.getCrimeStats(crimeList[i]).money;
-				stats /= time;
-				if (stats > optimal) {
-					optimal = stats;
-					optimalCrime = crimeList[i];
-				}
-			}
-			return optimalCrime;
-		} else if (arg == "karma") {
-			for (let i = 0; i < crimeList.length; ++i) {
-				chance = ns.getCrimeChance(crimeList[i]);
-				if (chance < 0.8) {
-					continue;
-				}
-				time = ns.getCrimeStats(crimeList[i]).time;
-				stats = ns.getCrimeStats(crimeList[i]).karma;
-				stats /= time;
-				if (stats > optimal) {
-					optimal = stats;
-					optimalCrime = crimeList[i];
-				}
-			}
-			return optimalCrime;
+	function crime() {
+		if (ns.getCrimeChance("homicide") < 0.6) {
+			return "mug someone";
+		} else {
+			return "homicide";
 		}
 	}
 
 	async function trainStats() {
 		//get player stats
-		var statLevels = [ns.getPlayer().strength, ns.getPlayer().defense, ns.getPlayer().agility, ns.getPlayer().dexterity];
+		var statLevels = [ns.getPlayer().strength, ns.getPlayer().defense, ns.getPlayer().dexterity, ns.getPlayer().agility];
 		var gym = "powerhouse gym"; //assuming we're in sector-12
 
-		for (let i = 0; i < statNames.length; ++i) {
-			ns.print("Training " + statNames[i]);
+		for (let i = 0; i < statLevels.length;++i) {
 			while (statLevels[i] < 15) {
+				ns.print("Training " + statNames[i]);
 				ns.gymWorkout(gym, statNames[i]);
-				await ns.sleep(5000);
-				statLevels = [ns.getPlayer().strength, ns.getPlayer().defense, ns.getPlayer().agility, ns.getPlayer().dexterity];
+				await ns.sleep(1000);
+				statLevels = [ns.getPlayer().strength, ns.getPlayer().defense, ns.getPlayer().dexterity, ns.getPlayer().agility];
 			}
 		}
 		ns.stopAction();
@@ -117,18 +44,9 @@ export async function main(ns) {
 	}
 
 	//main
-	oldKarma = karma;
-	while (true) {
-		if (possibleArgs.includes(ns.args[0])) {
-			//checking for money or karma args
-			bestCrime = crime(ns.args[0]);
-		} else if (crimeList.includes(ns.args[0])) {
-			//checking for heistname arg
-			bestCrime = ns.args[0];
-		} else {
-			bestCrime = crime("noArg");
-		}
-
+	while (karma < 54000) {
+		bestCrime = crime();
+		
 		//get and format the time into 24h
 		var date = new Date(Date.now() + ns.getCrimeStats(bestCrime).time);
 		var time = ns.nFormat(date.getHours(), "00") + ":" + ns.nFormat(date.getMinutes(), "00") + ":" + ns.nFormat(date.getSeconds(), "00");
