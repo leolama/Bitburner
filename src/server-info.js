c8ef24bc5f3e38207fa681b780261845c6c071fe
leolama <leolama2@hotmail.co.uk>
2022-02-17 22:53:06 +0000

    stopped showing previous server

--- src/server-info.js
+++ src/server-info.js
@@ -1,25 +1,12 @@
 /** @param {import("../.").NS} ns */
 export async function main(ns) {
 	ns.print("Script started");
-	var hostserver = ns.scan(ns.getHostname());
 	var targetServer = ns.args[0];
 
-	ns.tprint("--" + targetServer + " stats--");
-	ns.tprint("Money available: " + ns.nFormat(ns.getServerMoneyAvailable(targetServer), "$0.000a"));
-	ns.tprint("Maximum money: " + ns.nFormat(ns.getServerMaxMoney(targetServer), "$0.000a"));
-	ns.tprint("Hacking level required: " + ns.getServerRequiredHackingLevel(targetServer));
-	ns.tprint("Current security level: " + ns.getServerSecurityLevel(targetServer));
-	ns.tprint("Minimum security level: " + ns.getServerMinSecurityLevel(targetServer));
-
-	await scan(hostserver, ns.getHostname());
-
-	function scan(servers, host) {
-		for (const server of servers) {
-			if (server === targetServer) {
-				ns.tprint("Previous server: " + host);
-			}
-			var moreservs = ns.scan(server);
-			moreservs.splice(moreservs.indexOf(host), 1);
-		}
-	}
+	ns.tprintf("--" + targetServer + " stats--");
+	ns.tprintf("Money available: " + ns.nFormat(ns.getServerMoneyAvailable(targetServer), "$0.000a"));
+	ns.tprintf("Maximum money: " + ns.nFormat(ns.getServerMaxMoney(targetServer), "$0.000a"));
+	ns.tprintf("Hacking level required: " + ns.getServerRequiredHackingLevel(targetServer));
+	ns.tprintf("Current security level: " + ns.getServerSecurityLevel(targetServer));
+	ns.tprintf("Minimum security level: " + ns.getServerMinSecurityLevel(targetServer));
 }
