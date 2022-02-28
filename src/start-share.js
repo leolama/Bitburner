8f713208276759bd33cd2663160c016e405c8598
leolama <leolama@hotmail.co.uk>
2022-02-28 16:13:39 +0000

    added error catching

--- src/start-share.js
+++ src/start-share.js
@@ -5,6 +5,9 @@ export async function main(ns) {
 	threads = ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
 	threads /= ns.getScriptRam("/src/share.js", "home");
 	threads = Math.floor(threads) - 8; //spares ~36GB RAM
-
-	ns.run("/src/share.js", threads);
+	try {
+		ns.run("/src/share.js", threads);
+	} catch {
+		ns.tprint("Not enough RAM available to run share.js")
+	}
 }
