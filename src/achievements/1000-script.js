84430afd3075c36a170353413934639a1becba40
leolama <leolama@hotmail.co.uk>
2022-03-05 02:54:33 +0000

    removed /src/achievements/

--- src/achievements/1000-script.js
+++
@@ -1,7 +0,0 @@
-/** @param {import("../../.").NS} ns */
-export async function main(ns) {
-	while (true) {
-		ns.print(ns.args[0]);
-		await ns.sleep(5000);
-	}
-}
--- src/achievements/alter.js
+++
@@ -1,5 +0,0 @@
-//exploit
-/** @param {import("../../.").NS} ns */
-export async function main(ns) {
-	ns.alterReality();
-}
