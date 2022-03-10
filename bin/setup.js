3510c0a1724c17895d690cf2bf62d7201122f6e8
leolama <leolama@hotmail.co.uk>
2022-03-10 00:08:20 +0000

    added player corp checking to start the corp manager

--- bin/setup.js
+++ bin/setup.js
@@ -4,13 +4,20 @@ import { getServerPath } from 'util.js';
 export async function main(ns) {
 	ns.print("Script started");
 	const factionServerNames = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "w0r1d_d43m0n"];
-	const scripts = ["hack-manager.js", "buy-manager.js", "faction-manager.js", "crime-manager.js", "sleeve-manager.js", "gang-manager.js", "stock-manager.js"];
+	const scripts = ["hack-manager.js", "buy-manager.js", "faction-manager.js", "crime-manager.js", "sleeve-manager.js", "stock-manager.js"];
     var scriptsRam = [];
     var execRam = ns.getScriptRam("bin/exec.js");
     var playerRam = ns.getServerMaxRam("home") - execRam;
 	var scriptsToStart = [];
 	var factionPaths = [];
 
+    if (ns.getPlayer().hasCorporation) {
+        //if we have a corporation start the manager for it
+        scripts.push('corp-manager.js')
+    } else {
+        //else overwrite the data file so we don't start from stage 4
+        await ns.write('/data/corp-stage.txt', '0', 'w')
+    }
 
 	//get required ram of each script
 	for (let script of scripts) {
@@ -19,11 +26,11 @@ export async function main(ns) {
         scriptsRam.push(ram);
 	}
 
+    //calculate how many scripts we can run with our system RAM
     for (let i in scriptsRam) {
         if (playerRam > scriptsRam[i])  {
             scriptsToStart.push("/managers/" + scripts[i]);
             playerRam = playerRam - scriptsRam[i];
-            ns.print(playerRam);
         }
     }
 
