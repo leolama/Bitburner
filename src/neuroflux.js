c65eccb9db1be6c36e7db1875b9655c393d0ece1
leolama <leolama@hotmail.co.uk>
2022-03-14 18:55:46 +0000

    added ns.flags()

--- src/neuroflux.js
+++ src/neuroflux.js
@@ -1,37 +1,50 @@
 import { log } from 'util.js'
 
+const scriptArgs = [
+    ['faction', 'CyberSec']
+];
+
+export function autocomplete(data, args) {
+    data.flags(scriptArgs);
+    return [];
+}
+
 /** @param {import("../.").NS} ns */
 export async function main(ns) {
 	ns.disableLog("ALL");
 	ns.print("Script started");
-	var faction = ns.args[0];
+	const flags = ns.flags(scriptArgs);
 	var availMoney = ns.getPlayer().money;
 	var augPrice = ns.getAugmentationPrice("NeuroFlux Governor");
 	var augRep = ns.getAugmentationRepReq("NeuroFlux Governor");
-	var playerFactionRep = ns.getFactionRep(faction);
 	var count = 0;
+
+	try {
+		var playerFactionRep = ns.getFactionRep(flags.faction);
+	} catch {
+		log(ns, 'ERROR: A faction called "' + flags.faction + '" does not exist', true);
+		return;
+	}
 
 	//if player has enough money and reputation
 	if (availMoney >= augPrice && playerFactionRep >= augRep) {
 		while (availMoney >= augPrice && playerFactionRep >= augRep) {
-			if (ns.purchaseAugmentation(faction, "NeuroFlux Governor")) {
+			await ns.sleep(500);
+			if (ns.purchaseAugmentation(flags.faction, "NeuroFlux Governor")) {
 				++count;
 			} else {
 				log(ns, "ERROR: Failed to buy NeuroFlux", true);
+				return;
 			}
 			//refresh money and rep
 			availMoney = ns.getPlayer().money;
 			augPrice = ns.getAugmentationPrice("NeuroFlux Governor");
 			augRep = ns.getAugmentationRepReq("NeuroFlux Governor");
-			playerFactionRep = ns.getFactionRep(faction);
+			playerFactionRep = ns.getFactionRep(flags.faction);
 		}
 	} else {
 		log(ns, "WARN: Not enough money and/or reputation", true);
 		return;
 	}
 	log(ns, "SUCCESS: Bought " + count + " level(s) of NeuroFlux", true);
-}
-
-export function autocomplete(data, args) {
-    return ["CyberSec", "NiteSec", "The Black Hand", "BitRunners", "Daedalus"];
 }
