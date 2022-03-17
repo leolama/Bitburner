4b8f978d8d56ca482d97ee19a661f2fc69df12e4
leolama <leolama@hotmail.co.uk>
2022-03-17 16:35:19 +0000

    added script flags, reworked the launching of other scripts

--- managers/faction-manager.js
+++ managers/faction-manager.js
@@ -2,25 +2,37 @@ import { hackTools, nukeServer, terminalInput, log, getNsDataThroughFile, runCom
 
 const doc = eval("document")
 
+const scriptArgs = [
+	['nogang', false],
+	['nocorp', false],
+	['nostocks', false]
+];
+
+export function autocomplete(data, args) {
+	data.flags(scriptArgs);
+	return [];
+}
+
+
 /** @param {import("../.").NS} ns */
 export async function main(ns) {
 	ns.disableLog("ALL");
 	ns.print("Script started");
 
 	async function checkFactionInvites() {
-		let factionInvites = await getNsDataThroughFile(ns, `ns.checkFactionInvitations()`, 'player-faction-invites.txt');
+		let factionInvites = await getNsDataThroughFile(ns, `ns.checkFactionInvitations()`, '/data/player-faction-invites.txt');
 		//check for invitations from main factions
 		for (let fac of factionNames) {
 			if (factionInvites.includes(fac)) {
 				await runCommand(ns, `ns.joinFaction('${fac}')`)
-				ns.print("Joined " + fac);
+				log(ns, "SUCCESS: Joined " + fac);
 			}
 		}
 		//check for invitations from other factions
 		for (let fac of otherFactionNames) {
 			if (factionInvites.includes(fac)) {
 				await runCommand(ns, `ns.joinFaction('${fac}')`)
-				ns.print("Joined " + fac);
+				log(ns, "SUCCESS: Joined " + fac);
 			}
 		}
 	}
@@ -31,34 +43,63 @@ export async function main(ns) {
 			log(ns, "WARN: Player isn't on the terminal screen")
 			while (doc.getElementById("terminal-input") == null) {
 				await checkFactionInvites();
+				await runOtherManagers();
 				await ns.sleep(100);
 			}
 		}
 	}
 
-	function runOtherManagers() {
-		//since this script will be running until we finish the bitnode, we use this to run the gang and corporation managers
-		let karma = ns.heart.break();
-        if (karma < -54000) {
-            if (ns.run('/managers/gang-manager.js') > 0) {
-                log(ns, 'INFO: Starting gang-manager.js');
-            }
-        }
+	async function runOtherManagers() {
+		//since this script will be running until we finish the bitnode, we use this to run some other managers
+		//gangs
+		if (skipManagers.gang == false) {
+			let karma = ns.heart.break();
+			if (karma < -54000) {
+				let checkProcess = (await getNsDataThroughFile(ns, `ns.ps()`, '/data/process-list.txt')).filter(p => p.filename == '/managers/gang-manager.js');
+				if (checkProcess.length > 0) {
+					log(ns, 'WARN: Another version of /managers/gang-manager.js is already running, skipping');
+					skipManagers.gang = true;
+				} else {
+					log(ns, 'SUCCESS: Starting gang-manager.js');
+					ns.run('/managers/gang-manager.js');
+					skipManagers.gang = true;
+				}
+			}
+		}
 
-		if (ns.getPlayer().money > 150e9) {
-            if (ns.run('/managers/corp-manager.js') > 0) {
-                log(ns, 'INFO: Starting corp-manager.js');
-            }
-        }
+		//corp
+		if (skipManagers.corp == false) {
+			if (ns.getPlayer().money > 150e9) {
+				let checkProcess = (await getNsDataThroughFile(ns, `ns.ps()`, '/data/process-list.txt')).filter(p => p.filename == '/managers/corp-manager.js');
+				if (checkProcess.length > 0) {
+					log(ns, 'WARN: Another version of /managers/corp-manager.js is already running, skipping');
+					skipManagers.corp = true;
+				} else {
+					log(ns, 'SUCCESS: Starting corp-manager.js');
+					ns.run('/managers/corp-manager.js');
+					skipManagers.corp = true;
+				}
+			}
+		}
 
-		if (ns.getPlayer().hasWseAccount && ns.getPlayer().hasTixApiAccess) {
-			//if we have a stocks account and API acces then start the manager for it
-			if (ns.run('/managers/stock-manager.js') > 0) {
-                log(ns, 'INFO: Starting stock-manager.js');
+		//stocks
+		if (skipManagers.stocks == false) {
+			if (ns.getPlayer().hasWseAccount && ns.getPlayer().hasTixApiAccess) {
+				//if we have a stocks account and API acces then start the manager for it
+				let checkProcess = (await getNsDataThroughFile(ns, `ns.ps()`, '/data/process-list.txt')).filter(p => p.filename == '/managers/stock-manager.js');
+				if (checkProcess.length > 0) {
+					log(ns, 'WARN: Another version of /managers/stock-manager.js is already running, skipping');
+					skipManagers.stocks = true;
+				} else {
+					log(ns, 'SUCCESS: Starting stock-manager.js');
+					ns.run('/managers/stock-manager.js');
+					skipManagers.stocks = true;
+				}
 			}
 		}
 	}
 
+	const flags = ns.flags(scriptArgs);
 	const factionNames = ["CyberSec", "NiteSec", "The Black Hand", "BitRunners", "w0r1d_d43m0n"]; //backdoor based factions
 	const otherFactionNames = [
 		"Daedalus",
@@ -92,6 +133,26 @@ export async function main(ns) {
 	var count = 0;
 	var hackingLvl = ns.getPlayer().hacking; //player hacking level
 	var playerAugs = ns.read('/data/player-augs-purchased.txt');
+
+	//function vars
+	var skipManagers = {
+		gang:false,
+		corp:false,
+		stocks:false
+	}
+
+	if (flags.nogang) {
+		skipManagers.gang = true
+		log(ns, 'WARN: Skipping gang-manager.js')
+	}
+	if (flags.nocorp) {
+		skipManagers.corp = true
+		log(ns, 'WARN: Skipping corp-manager.js')
+	}
+	if (flags.nostocks) {
+		skipManagers.stocks = true
+		log(ns, 'WARN: Skipping stock-manager.js')
+	}
 
 	//get the paths to the faction servers
 	var temp_factionPaths = ns.read('/data/faction-paths.txt');
@@ -133,13 +194,21 @@ export async function main(ns) {
 						await terminalInput("home");
 					}
 				} else {
-					log(ns, "WARN: Trying to backdoor " + factionServerNames[count] + ". Need hacking level " + factionHackLvl[count] + ", have " + hackingLvl + ". Need " + factionTools[count] + " tools, have " + numTools);
+					log(ns, "WARN: Trying to backdoor " + factionServerNames[count] + ". Need hacking level " + factionHackLvl[count] + ", and " + factionTools[count] + " hacking tools");
+					while (hackingLvl <= factionHackLvl[count] || numTools <= factionTools[count]) {
+						//reduce log spam and refresh vars
+						numTools = hackTools(ns);
+						hackingLvl = ns.getPlayer().hacking;
+						await checkFactionInvites();
+						await runOtherManagers();
+						await ns.sleep(1000);
+					}
 				}
 				//refresh vars
 				numTools = hackTools(ns);
 				hackingLvl = ns.getPlayer().hacking;
 				await checkFactionInvites();
-				runOtherManagers();
+				await runOtherManagers();
 				await ns.sleep(1000);
 			}
 		} else {
